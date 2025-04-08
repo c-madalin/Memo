795088a4f9d9846ed05fb273f569c9f75c328605
[cazan] <cazanmadalin12@gmail.com>
2025-04-08 21:49:19 +0300

    User interface
    -nuget package
    -new user window

---
+++ MemoGame/MemoGame/ViewModels/NewUserDialogViewModel.cs
@@ -0,0 +1,64 @@
+﻿using MemoGame.Commands;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace MemoGame.ViewModels
+{
+    public class NewUserDialogViewModel : BaseViewModel
+    {
+        private string _username;
+        private string _selectedAvatar;
+        private int _currentAvatarIndex;
+        private List<string> _avatarPaths;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                SetProperty(ref _username, value);
+                OnPropertyChanged(nameof(CanCreate));
+            }
+        }
+
+        public string SelectedAvatar
+        {
+            get => _selectedAvatar;
+            set => SetProperty(ref _selectedAvatar, value);
+        }
+
+        public bool CanCreate => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrEmpty(SelectedAvatar);
+
+        public ICommand NextAvatarCommand { get; private set; }
+        public ICommand PreviousAvatarCommand { get; private set; }
+
+        public NewUserDialogViewModel()
+        {
+            _avatarPaths = new List<string>();
+            NextAvatarCommand = new RelayCommand(_ => CycleAvatar(1));
+            PreviousAvatarCommand = new RelayCommand(_ => CycleAvatar(-1));
+        }
+
+        public void SetAvatarPaths(List<string> avatarPaths)
+        {
+            _avatarPaths = avatarPaths;
+
+            // Set default avatar if available
+            if (_avatarPaths.Count > 0)
+            {
+                _currentAvatarIndex = 0;
+                SelectedAvatar = _avatarPaths[0];
+            }
+        }
+
+        private void CycleAvatar(object parameter)
+        {
+            if (_avatarPaths.Count == 0)
+                return;
+
+            int direction = (int)parameter;
+            _currentAvatarIndex = (_currentAvatarIndex + direction + _avatarPaths.Count) % _avatarPaths.Count;
+            SelectedAvatar = _avatarPaths[_currentAvatarIndex];
+        }
+    }
+}
---
+++ MemoGame/MemoGame/Views/NewUserDialogWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace MemoGame.Views
+{
+    /// <summary>
+    /// Interaction logic for NewUserDialogWindow.xaml
+    /// </summary>
+    public partial class NewUserDialogWindow : Window
+    {
+        public NewUserDialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
